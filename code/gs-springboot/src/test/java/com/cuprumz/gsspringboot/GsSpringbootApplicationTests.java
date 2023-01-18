548f96b380a7874cede074d06d43cc7ebe9ad903
cuprumz <cuprumzhen@gmail.com>
2023-01-18 17:20:24 +0800

    feat(code) initial spring boot demo

---
+++ code/gs-springboot/src/test/java/com/cuprumz/gsspringboot/GsSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cuprumz.gsspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
