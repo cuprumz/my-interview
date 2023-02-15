1f40c47c98ce0bb9061d4c8f2e31ff1abe04dd04
cuprumz <cuprumzhen@gmail.com>
2023-02-15 10:04:02 +0800

    test: java datasource hikari impl

--- code/gs-springboot/src/test/java/com/cuprumz/db/JDBCApp.java
+++ code/gs-springboot/src/test/java/com/cuprumz/db/JDBCApp.java
@@ -1,13 +1,21 @@
 package com.cuprumz.db;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import org.junit.jupiter.api.Test;
 
+import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
 public class JDBCApp {
+
+    private final String LocalMySQLUrl = "jdbc:mysql://localhost:3306/test";
+    private final String username = "root";
+    private final String passwd = "root";
+    
 
     @Test
     public void getConn() {
@@ -16,9 +24,9 @@ public class JDBCApp {
             Class.forName("com.mysql.cj.jdbc.Driver");
 
             Connection conn = DriverManager.getConnection(
-                    "jdbc:mysql://localhost:3306/test"
-                    , "root"
-                    , "root");
+                    LocalMySQLUrl
+                    , username
+                    , passwd);
 
             PreparedStatement ps = conn.prepareStatement("select @@version");
 
@@ -30,5 +38,29 @@ public class JDBCApp {
         } catch (Exception ignore) {
 
         }
+    }
+    
+    
+    @Test
+    public void pooledConn() throws Exception {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(LocalMySQLUrl);
+        config.setUsername(username);
+        config.setPassword(passwd);
+        DataSource ds = new HikariDataSource(config);
+
+        Connection conn = ds.getConnection();
+        
+        PreparedStatement ps = conn.prepareStatement("select @@version");
+
+        ResultSet rs = ps.executeQuery();
+
+        while (rs.next()) {
+            System.out.println(rs.getString(1));
+        }
+        
+        rs.close();
+        ps.close();
+        conn.close();
     }
 }
