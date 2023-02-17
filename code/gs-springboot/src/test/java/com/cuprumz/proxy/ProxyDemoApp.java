f978dd9c229bebbd75b281102f77607418460e7f
cuprumz <cuprumzhen@gmail.com>
2023-02-17 15:55:20 +0800

    test: proxy invocation handler and optional

---
+++ code/gs-springboot/src/test/java/com/cuprumz/proxy/ProxyDemoApp.java
@@ -0,0 +1,12 @@
+package com.cuprumz.proxy;
+
+import org.junit.jupiter.api.Test;
+
+public class ProxyDemoApp {
+    
+    
+    @Test
+    public void getStarted() {
+        
+    }
+}
---
+++ code/gs-springboot/src/test/java/lib/guava/basicoutilities/UsingAndAvoidingNullExplainedApp.java
@@ -0,0 +1,13 @@
+package lib.guava.basicoutilities;
+
+import com.google.common.base.Optional;
+import org.junit.jupiter.api.Test;
+
+
+public class UsingAndAvoidingNullExplainedApp {
+    
+    @Test
+    public void optional() {
+        Optional.of(5);
+    }
+}
